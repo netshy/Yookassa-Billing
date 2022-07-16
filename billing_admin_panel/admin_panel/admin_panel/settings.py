feed5eb96877ec6b3aafba35408619e1252d2f40
nonameists <nonameists@gmail.com>
2022-07-16 18:40:22 +0300

    add model to admin page add __str__ to models add default User model with diff table_name

--- billing_admin_panel/admin_panel/admin_panel/settings.py
+++ billing_admin_panel/admin_panel/admin_panel/settings.py
@@ -90,3 +90,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'subscription.User'
--- billing_admin_panel/admin_panel/subscription/models.py
+++ billing_admin_panel/admin_panel/subscription/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from billing_admin_panel.admin_panel.subscription.model_mixins import (
+from subscription.model_mixins import (
     UUIDMixin,
     CurrencyType,
     SubscriptionPlanStatus,
@@ -15,10 +16,20 @@ class SubscriptionPlan(UUIDMixin):
     name = models.CharField(max_length=250)
     price = models.IntegerField()
     duration = models.IntegerField()
-    currency = models.CharField(choices=CurrencyType.choices, default=CurrencyType.USD)
-    status = models.CharField(
-        choices=SubscriptionPlanStatus.choices, default=SubscriptionPlanStatus.ACTIVE
+    currency = models.CharField(
+        choices=CurrencyType.choices, default=CurrencyType.USD, max_length=10
     )
+    status = models.CharField(
+        choices=SubscriptionPlanStatus.choices,
+        default=SubscriptionPlanStatus.ACTIVE,
+        max_length=100,
+    )
+
+    class Meta:
+        db_table = "subscription_plan"
+
+    def __str__(self):
+        return f"Subscription Plan: {self.name}. Cost: {self.price}"
 
 
 class Subscription(UUIDMixin, TimeStampedMixin):
@@ -31,8 +42,16 @@ class Subscription(UUIDMixin, TimeStampedMixin):
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
     status = models.CharField(
-        choices=SubscriptionStatus.choices, default=SubscriptionStatus.ACTIVE
+        choices=SubscriptionStatus.choices,
+        default=SubscriptionStatus.ACTIVE,
+        max_length=100,
     )
+
+    class Meta:
+        db_table = "subscription"
+
+    def __str__(self):
+        return f"Subscription: {self.id} for customer id: {self.customer_id}"
 
 
 class Transaction(UUIDMixin):
@@ -44,8 +63,16 @@ class Transaction(UUIDMixin):
     )
     session_id = models.TextField()
     status = models.CharField(
-        choices=TransactionStatus.choices, default=TransactionStatus.PROCESSING
+        choices=TransactionStatus.choices,
+        default=TransactionStatus.PROCESSING,
+        max_length=100,
     )
+
+    class Meta:
+        db_table = "transaction"
+
+    def __str__(self):
+        return f"Transaction: {self.id} for customer: {self.customer_id}. Status: {self.status}"
 
 
 class Refund(UUIDMixin, TimeStampedMixin):
@@ -57,5 +84,19 @@ class Refund(UUIDMixin, TimeStampedMixin):
     )
     amount = models.IntegerField()
     status = models.CharField(
-        choices=RefundStatus.choices, default=RefundStatus.PROCESSING
+        choices=RefundStatus.choices, default=RefundStatus.PROCESSING, max_length=100
     )
+
+    class Meta:
+        db_table = "refund"
+
+    def __str__(self):
+        return (
+            f"Refund for customer id: {self.customer_id}. Transaction id: {self.transaction_id}. "
+            f"Amount: {self.amount}. Status: {self.status}"
+        )
+
+
+class User(AbstractUser):
+    class Meta:
+        db_table = "user"

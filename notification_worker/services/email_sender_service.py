e8223a18eea8bc174a98717285cde1f581248b45
Ivan Lundak <i.lundak@magnus-tech.ru>
2022-07-29 17:15:39 +0300

    auth mailgun

--- notification_worker/services/email_sender_service.py
+++ notification_worker/services/email_sender_service.py
@@ -6,7 +6,7 @@ from services.mailgun_config import mailgun_config
 class EmailSenderService:
     def send_email(self, to, subject, body) -> int:
         """method send email notification."""
-        authentication = {"api": mailgun_config.mailgun_api_key}
+        authentication = ("api", mailgun_config.mailgun_api_key)
         data = {
             "from": mailgun_config.mailgun_sandbox_from_email,
             "to": to,

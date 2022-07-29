f0b82d5d470b571de03ad2b3964bae89a1c2d533
Ivan Lundak <i.lundak@magnus-tech.ru>
2022-07-29 11:34:03 +0300

    fix naming

--- billing_admin_panel/admin_panel/entrypoint.sh
+++ billing_admin_panel/admin_panel/entrypoint.sh
@@ -5,7 +5,7 @@ while ! nc -z "$DB_HOST" "$DB_PORT"; do sleep 1; done;
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-python manage.py loaddata fixtures/fixture.json
+python manage.py loaddata fixtures/admin_panel_data.json
 python -m gunicorn admin_panel.wsgi:application --bind 0.0.0.0:9000
 
 exec "$@"

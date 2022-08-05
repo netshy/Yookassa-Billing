81f81229eba25233f59058dac244a0cfe7757e6a
Ivan Lundak <i.lundak@magnus-tech.ru>
2022-08-05 10:42:00 +0300

    heartbeat increase

--- notification_worker/rabbimq.py
+++ notification_worker/rabbimq.py
@@ -23,7 +23,7 @@ class PikaClient:
             config.rabbit_host,
             config.rabbit_port,
             credentials=self.credentials,
-            heartbeat=60,
+            heartbeat=600,
         )
         self.connection = self._connect()
         self.channel = self.connection.channel()
